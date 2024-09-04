45c331eb404fbfeaa49f1ed5d30fe4e9164c06cb
Jamie Schouten <j4mie@hey.com>
2024-09-04 21:23:52 +0200

    clean ignore

--- workbench/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require '../examples/advanced-line-chart.php';
-require '../examples/simple-line-chart.php';
-require '../examples/advanced-bar-chart.php';
-require '../examples/simple-bar-chart.php';
