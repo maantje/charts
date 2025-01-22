7bdaa30b2f666fbb5171b1e54704906f02d69a04
Jamie Schouten <j4mie@hey.com>
2025-01-22 22:28:54 +0100

    Allow adding a SVG viewBox (#15)

--- tests/Unit/CurvedLineChartTest.php
+++ tests/Unit/CurvedLineChartTest.php
@@ -36,7 +36,7 @@ it('renders curved line chart', function () {
     );
 
     expect(pretty($chart->render()))->toBe(<<<'SVG'
-<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600">
+<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600" viewBox="0 0 800 600">
   <rect x="0" y="0" width="800" height="600" fill="white" fill-opacity="1" stroke="none" stroke-width="0" rx="0" ry="0">
     <title/>
   </rect>
@@ -102,9 +102,8 @@ it('renders empty line chart', function () {
             ),
         ],
     );
-
     $svg = <<<'SVG'
-<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600">
+<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600" viewBox="0 0 800 600">
   <rect x="0" y="0" width="800" height="600" fill="white" fill-opacity="1" stroke="none" stroke-width="0" rx="0" ry="0">
     <title/>
   </rect>
--- tests/Unit/GroupedBarChartTest.php
+++ tests/Unit/GroupedBarChartTest.php
@@ -109,7 +109,7 @@ it('renders grouped bar chart', function () {
     echo $chart->render();
 
     expect(pretty($chart->render()))->toBe(<<<'SVG'
-<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600">
+<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600" viewBox="0 0 800 600">
   <rect x="0" y="0" width="800" height="600" fill="white" fill-opacity="1" stroke="none" stroke-width="0" rx="0" ry="0">
     <title/>
   </rect>
@@ -206,7 +206,7 @@ it('renders empty grouped bar chart', function () {
     );
 
     expect(pretty($chart->render()))->toBe(<<<'SVG'
-<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600">
+<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600" viewBox="0 0 800 600">
   <rect x="0" y="0" width="800" height="600" fill="white" fill-opacity="1" stroke="none" stroke-width="0" rx="0" ry="0">
     <title/>
   </rect>
